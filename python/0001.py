7b424f9334291702c1fa34073d333511643b672c
Byron Slater <byron.slater3@googlemail.com>
2017-11-30 03:40:46 +0000

    First commit

---
+++ python/0001.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.6
+def get_answer():
+    tot = 0
+    for i in range(1000):
+        if i % 3 == 0 or i % 5 == 0:
+            tot += i
+
+    return tot
+
+
+if __name__ == '__main__':
+    print(get_answer())
